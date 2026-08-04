20a4a228ad001a2e4ebe7c760b6668b3168590d2
AllanWijaya <allan.wjy450@gmail.com>
2026-08-04 19:44:17 +0700

    Delete method POST

--- pages/api/pdf.ts
+++ pages/api/pdf.ts
@@ -4,12 +4,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== "GET") {
-    return res.status(405).json({
-      message: "Method Not Allowed",
-    });
-  }
-
   const { invoiceData, brandData, printOptions } = req.body;
 
   if (!invoiceData) {
